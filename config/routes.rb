5262aad9cedf6842c35aad177a9254a105aceafa
Ryan <junheng102@gmail.com>
2018-02-07 13:13:12 +0800

    Change root path to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
+  # Registrations controller will overwrite default devise route when user register for an account.
   devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
