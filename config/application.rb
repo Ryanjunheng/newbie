cb2ea309c4f2d92abfac03ba3bd6bce123b744f9
Ryan <junheng102@gmail.com>
2018-02-07 12:51:53 +0800

    Configure actionmailer smtp settings for development purpose

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,16 @@ module Newbie
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Action mailer smtp settings:
+    ActionMailer::Base.smtp_settings = {
+    	:address              => "smtp.gmail.com",
+    	:domain				  => "mail.google.com",
+   		:port                 => 587,
+   		:user_name            => ENV['gmail_username'],
+   		:password             => ENV['gmail_password'],
+   		:authentication       => "login",
+    	:enable_starttls_auto => true
+    }
   end
 end
